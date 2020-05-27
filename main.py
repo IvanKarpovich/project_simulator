fdf083c6fcc44e97e741020cd97748210cba2f18
IvanKarpovich <Gleb123@sibmail.com>
2020-05-27 23:46:32 +0700

    Отредактировал комментарии

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from matplotlib.colors import LinearSegmentedColormap
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 import glob, os
 from PIL import Image
 
+'''
+Создание директории
+'''
 try:
     os.mkdir("frames")
 except OSError:
@@ -26,68 +28,56 @@ for file in glob.glob('frames/*'):
 fp_in = "frames/step*.png"
 fp_out = "image.gif"
 
+x_r = np.arange(-1, 1, 0.01)
+y_r = np.arange(-1, 1, 0.01)
+    
+xgrid, ygrid = np.meshgrid(x_r, y_r)
 '''
 Создание источника(координаты x и y, длина волны и период)
 E(r, t)=E_0*cos(k*r + w*t + phi_0)
 '''
-def source(x, y, l, t, phi):
-    x_g = xgrid-x
-    y_g = ygrid-y
+def create_new_source(x, y, l, t, phi):
+    x_shift = xgrid-x
+    y_shift = ygrid-y
+
+    global data
+    radius = np.sqrt(x_shift*x_shift + y_shift*y_shift)
+    data = np.cos(-radius*2*np.pi/l + 2*np.pi/t*time_wave + phi)
     
-    r = np.sqrt(x_g*x_g + y_g*y_g)
-    data = np.cos(-r*2*math.pi/l + 2*math.pi/t*i + phi)
-    data_x = x_g/r*data
-    data_y = y_g/r*data
+    global data_x
+    global data_y
+    data_x = x_shift/radius*data
+    data_y = y_shift/radius*data
     
     global dataX_0
     global dataY_0
     dataX_0 = dataX_0+data_x
     dataY_0 = dataY_0+data_y
-'''
-Зеркальная поверхность с координатами
-'''
-'''
-def mirrow(x_1, y_1, x_2, y_2):
-    x_1 = xgrid-0.5
-    global data
-    data += np.cos(-np.sqrt(x_1*x_1+ygrid*ygrid)*2*math.pi/lam+2*math.pi/T*i+math.pi)
-'''
-'''
-Неотражающая поверхность
 
-
-def wall(x, y, x_1, y_1, x_2, y_2):
-    if():
-     
-    
-    
-fig = plt.figure()
-
-
+'''
 Параметры
 '''
-par_l = 1/5
-par_t = 100
+PAR_L = 1/3
+PAR_T = 100
 
 '''
 Рисование N картинок
 '''
-for i in range (0, 100, 5):
-    x_r = np.arange(-1, 1, 0.01)
-    y_r = np.arange(-1, 1, 0.01)
-    
-    xgrid, ygrid = np.meshgrid(x_r, y_r)
+for time_wave in range (0, 5, 5):
     dataX_0 = xgrid-xgrid
     dataY_0 = xgrid-xgrid
     
-    source(0, 0, par_l, par_t, 0)
-    source(0, 0, par_l, par_t, math.pi)
+    create_new_source(0, 0, PAR_L, PAR_T, 0)
 
-    plt.imshow(np.sqrt(dataX_0**2+dataY_0**2),cmap=cm.seismic)
+    #plt.imshow(np.sqrt(dataX_0**2+dataY_0**2),cmap=cm.seismic)
+    plt.imshow(np.sqrt(data_x**2+data_y**2),cmap=cm.seismic)
+    plt.show()
+    plt.imshow(data, cmap=cm.seismic)
 
-    filename = 'frames/step'+str(100000+i)[1:]+'.png'
-    plt.savefig(filename, dpi=96)
+    filename = 'frames/step'+str(100000+time_wave)[1:]+'.png'
+    plt.savefig(filename, dpi=100)
     plt.gca()
+    plt.show()
     
 '''
 Создание GIF из картинок
